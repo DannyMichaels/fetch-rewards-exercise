77a63b45678dd08488ddf41570039c247a0b567b
Daniel <itzdanielmichael@gmail.com>
2021-03-16 17:46:42 -0400

    move footer.jsx

--- src/components/shared/layout/Layout.jsx
+++ src/components/shared/layout/Layout.jsx
@@ -1,4 +1,4 @@
-import Footer from '../../Footer/Footer';
+import Footer from './Footer/Footer';
 import Header from './Header/Header';
 
 const Layout = ({ title, children, setSearch, cartState }) => (
